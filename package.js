e088acde2ac7ef6aa91da16f0d12e88881045a04
Werner Bihl <wernerbihl@gmail.com>
2017-07-18 23:49:14 -0700

    Bumping version number

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'innovationswitch:meteor.adfs-oauth',
-  version: '0.0.4',
-  summary: 'Oauth2 authentication using Microsoft ADFS3 Oauth service (Active Directory) that allows for a custom callback URL',
+  version: '0.0.5',
+  summary: 'Oauth2 authentication using Microsoft ADFS3 Oauth service (Active Directory)',
   git: 'https://github.com/InnovationSwitch/meteor.adfs-oauth',
   documentation: 'README.md'
 });
