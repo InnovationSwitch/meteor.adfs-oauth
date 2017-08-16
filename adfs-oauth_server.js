9825135d60959bae85a08e6b6476d6550fdf4090
Werner Bihl <wernerbihl@gmail.com>
2017-08-16 04:40:40 -0700

    Reverting to 0.0.6 basically

--- adfs-oauth_server.js
+++ adfs-oauth_server.js
@@ -1,58 +1,41 @@
-console.log('ANALYTICS V3');
-
 Adfsoauth = {};
 
 OAuth.registerService('adfsoauth', 2, null, function (query) {
 
-    HTTP.call( 'GET', 'http://innovationswitch.tk/lookup.php', {}, function( error, res ) {
+    if (typeof Session != 'undefined' && Session.get('companyId') != 'undefined') {
+        var config = Companies.findOne({_id: Session.get('companyId')});
+    } else {
+        var config = ServiceConfiguration.configurations.findOne({service: 'adfsoauth'});
+    }
 
-      console.log('RESPONSE');
-      console.log(res);
+    if (!config) {
+        throw new ServiceConfiguration.ConfigError();
+    }
 
-      var subdomain = res.content.trim().toString();
+    var response = getTokens(query);
+    var expiresAt = (+new Date) + (1000 * parseInt(response.expiresIn, 10));
+    var accessToken = response.accessToken;
 
-      console.log('SUBDOMAIN');
-      console.log(subdomain);
+    var identity = getIdentity(accessToken);
 
-      if (typeof subdomain != 'undefined') {
-          var config = Companies.findOne({slug: subdomain});
-          console.log('CORRECT COMPANY: ' + subdomain);
-      } else {
-          var config = ServiceConfiguration.configurations.findOne({service: 'adfsoauth'});
-          console.log('INCORRECT COMPANY: ' + subdomain);
-      }
+    var serviceData = {
+        accessToken: accessToken,
+        expiresAt: expiresAt
+    };
 
-      if (!config) {
-          throw new ServiceConfiguration.ConfigError();
-      }
+    //Add fields from jwt token to the serviceData
+    _.extend(serviceData, identity);
 
-      var response = getTokens(query);
-      var expiresAt = (+new Date) + (1000 * parseInt(response.expiresIn, 10));
-      var accessToken = response.accessToken;
-      var identity = getIdentity(accessToken);
+    // only set the token in serviceData if it's there. this ensures
+    // that we don't lose old ones (since we only get this on the first
+    // log in attempt)
+    if (response.refreshToken)
+        serviceData.refreshToken = response.refreshToken;
 
-      var serviceData = {
-          accessToken: accessToken,
-          expiresAt: expiresAt
-      };
-
-      //Add fields from jwt token to the serviceData
-      _.extend(serviceData, identity);
-
-      // only set the token in serviceData if it's there. this ensures
-      // that we don't lose old ones (since we only get this on the first
-      // log in attempt)
-      if (response.refreshToken)
-          serviceData.refreshToken = response.refreshToken;
-
-      return {
-          serviceData: serviceData,
-          options: {profile: {name: identity[config.profileNameField]}}
-      };
-
-    });
-
-
+    return {
+        serviceData: serviceData,
+        options: {profile: {name: identity[config.profileNameField]}}
+    };
 });
 
 // returns an object containing:
@@ -61,94 +44,68 @@ OAuth.registerService('adfsoauth', 2, null, function (query) {
 // - refreshToken, if this is the first authorization request
 var getTokens = function (query) {
 
-    HTTP.call( 'GET', 'http://innovationswitch.tk/lookup.php', {}, function( error, res ) {
+    if (typeof Session != 'undefined' && Session.get('companyId') != 'undefined') {
+        var config = Companies.findOne({_id: Session.get('companyId')});
+    } else {
+        var config = ServiceConfiguration.configurations.findOne({service: 'adfsoauth'});
+    }
 
-      console.log('RESPONSE');
-      console.log(res);
+    if (!config) {
+        throw new ServiceConfiguration.ConfigError();
+    }
 
-      var subdomain = res.content.trim().toString();
+    var response;
+    try {
+        response = HTTP.post(
+            config.oauthAdfsUrl + "/token", {
+                npmRequestOptions: {
+                    rejectUnauthorized: false //allow self signed certificates
+                },
+                params: {
+                    code: query.code,
+                    client_id: config.clientId,
+                    client_secret: OAuth.openSecret(config.secret),
+                    redirect_uri: config.redirectUri,
+                    grant_type: 'authorization_code'
+                }
+            });
 
-      console.log('SUBDOMAIN');
-      console.log(subdomain);
+    } catch (err) {
+        console.log(err);
+        throw _.extend(new Error("Failed to complete OAuth handshake with Adfsoauth. " + err.message),
+            {response: err.response});
+    }
 
-      if (typeof subdomain != 'undefined') {
-          var config = Companies.findOne({slug: subdomain});
-          console.log('CORRECT COMPANY: ' + subdomain);
-      } else {
-          var config = ServiceConfiguration.configurations.findOne({service: 'adfsoauth'});
-          console.log('INCORRECT COMPANY: ' + subdomain);
-      }
-
-      if (!config) {
-          throw new ServiceConfiguration.ConfigError();
-      }
-
-      var response;
-      try {
-          response = HTTP.post(
-              config.oauthAdfsUrl + "/token", {
-                  npmRequestOptions: {
-                      rejectUnauthorized: false //allow self signed certificates
-                  },
-                  params: {
-                      code: query.code,
-                      client_id: config.clientId,
-                      client_secret: OAuth.openSecret(config.secret),
-                      redirect_uri: config.redirectUri,
-                      grant_type: 'authorization_code'
-                  }
-              });
-
-      } catch (err) {
-          console.log(err);
-          throw _.extend(new Error("Failed to complete OAuth handshake with Adfsoauth. " + err.message),
-              {response: err.response});
-      }
-
-      if (response.data.error) { // if the http response was a json object with an error attribute
-          throw new Error("Failed to complete OAuth handshake with Adfsoauth. " + response.data.error);
-      } else {
-          return {
-              accessToken: response.data.access_token,
-              refreshToken: response.data.refresh_token,
-              expiresIn: response.data.expires_in,
-              idToken: response.data.id_token
-          };
-      }
-    });
+    if (response.data.error) { // if the http response was a json object with an error attribute
+        throw new Error("Failed to complete OAuth handshake with Adfsoauth. " + response.data.error);
+    } else {
+        return {
+            accessToken: response.data.access_token,
+            refreshToken: response.data.refresh_token,
+            expiresIn: response.data.expires_in,
+            idToken: response.data.id_token
+        };
+    }
 };
 
 var getIdentity = function (accessToken) {
 
-    HTTP.call( 'GET', 'http://innovationswitch.tk/lookup.php', {}, function( error, res ) {
+    if (typeof Session != 'undefined' && Session.get('companyId') != 'undefined') {
+        var config = Companies.findOne({_id: Session.get('companyId')});
+    } else {
+        var config = ServiceConfiguration.configurations.findOne({service: 'adfsoauth'});
+    }
 
-      console.log('RESPONSE');
-      console.log(res);
+    if (!config) {
+        throw new ServiceConfiguration.ConfigError();
+    }
 
-      var subdomain = res.content.trim().toString();
-
-      console.log('SUBDOMAIN');
-      console.log(subdomain);
-
-      if (typeof subdomain != 'undefined') {
-          var config = Companies.findOne({slug: subdomain});
-          console.log('CORRECT COMPANY: ' + subdomain);
-      } else {
-          var config = ServiceConfiguration.configurations.findOne({service: 'adfsoauth'});
-          console.log('INCORRECT COMPANY: ' + subdomain);
-      }
-
-      if (!config) {
-          throw new ServiceConfiguration.ConfigError();
-      }
-
-      try {
-          return verifyToken(accessToken, config.publicCertPath, 'RS256');
-      } catch (err) {
-          throw _.extend(new Error("Failed to fetch identity from ADFS jwt token. " + err.message),
-              {response: err.response});
-      }
-    });
+    try {
+        return verifyToken(accessToken, config.publicCertPath, 'RS256');
+    } catch (err) {
+        throw _.extend(new Error("Failed to fetch identity from ADFS jwt token. " + err.message),
+            {response: err.response});
+    }
 };
 
 Adfsoauth.retrieveCredential = function (credentialToken, credentialSecret) {
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'innovationswitch:meteor.adfs-oauth',
-  version: '0.0.9',
+  version: '0.0.10',
   summary: 'Oauth2 authentication using Microsoft ADFS3 Oauth service (Active Directory)',
   git: 'https://github.com/InnovationSwitch/meteor.adfs-oauth',
   documentation: 'README.md'
