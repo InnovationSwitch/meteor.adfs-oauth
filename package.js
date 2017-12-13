55e98baa58dabf952ab299b0a83d4cb1208eccd3
Werner Bihl <wernerbihl@gmail.com>
2017-12-13 12:37:51 +0200

    Fixed IE 10 issue

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'innovationswitch:meteor.adfs-oauth',
-  version: '0.0.16',
+  version: '0.0.20',
   summary: 'Oauth2 authentication using Microsoft ADFS3 Oauth service (Active Directory)',
   git: 'https://github.com/InnovationSwitch/meteor.adfs-oauth',
   documentation: 'README.md'
@@ -28,6 +28,7 @@ Package.onUse(function(api) {
   api.addFiles('adfs-oauth_server.js', 'server');
   api.addFiles('adfs-oauth_client.js', 'client');
   api.addFiles('adfs-oauth.js');
+  api.addFiles('oauth_browser.js');
 
 });
 
